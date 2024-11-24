6a65cfd79add3e6e21e9c7c1486ad4d43484870e
Orkhand4 <orkhand4@mail.ru>
2024-11-24 13:00:42 +0400

    part2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,21 @@
-import React, { useEffect, useRef } from 'react'
-import Header from './components/header'
-import Animation from './animation'
+import React, { useEffect, useRef } from "react";
+import Header from "./components/header";
+import Animation from "./animation";
+import { coffees } from "./data/coffees.";
 
 const App = () => {
-
   const demoImg = useRef();
   const txt1 = useRef();
   const txt2 = useRef();
   const txt3 = useRef();
 
   useEffect(() => {
-    Animation(demoImg.current,-80,0,0,0,0,1,.2,-30,0)
+    Animation(demoImg.current, -80, 0, 0, 0, 0, 1, 0.2, -30, 0);
     Animation(txt1.current, 20, 0, 0, 0, 0, 1, 0.4);
     Animation(txt2.current, 20, 0, 0, 0, 0, 1, 0.6);
     Animation(txt3.current, 20, 0, 0, 0, 0, 1, 0.8);
-  },[])
+  }, []);
+
 
   return (
     <div>
@@ -38,18 +39,59 @@ const App = () => {
 
       <div className="min-h-screen bg-teal-800 py-40">
         <div className="container mx-auto flex items-center justify-center gap-24">
-          <div className='w-full h-full max-w-xl'>
-            <img ref={demoImg} className='w-full h-full object-cover' src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/640px-Starbucks_Corporation_Logo_2011.svg.png" />
+          <div className="w-full h-full max-w-xl">
+            <img
+              ref={demoImg}
+              className="w-full h-full object-cover"
+              src="https://upload.wikimedia.org/wikipedia/en/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/640px-Starbucks_Corporation_Logo_2011.svg.png"
+            />
           </div>
-          <div className=' text-white flex flex-col gap-8'>
-            <h2 ref={txt1} className="text-4xl  font-bold tracking-wider leading-[120%]">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
-            <p ref={txt2} className=" text-gray-200 text-2xl font-normal tracking-wider leading-[120%]"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis cum quia nobis sed beatae incidunt.</p>
-            <p ref={txt3} className=" text-gray-200 text-2xl font-normal tracking-wider leading-[120%]"> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis cum quia nobis sed beatae incidunt.</p>
+          <div className=" text-white flex flex-col gap-8">
+            <h2
+              ref={txt1}
+              className="text-4xl  font-bold tracking-wider leading-[120%]"
+            >
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </h2>
+            <p
+              ref={txt2}
+              className=" text-gray-200 text-2xl font-normal tracking-wider leading-[120%]"
+            >
+              {" "}
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
+              cum quia nobis sed beatae incidunt.
+            </p>
+            <p
+              ref={txt3}
+              className=" text-gray-200 text-2xl font-normal tracking-wider leading-[120%]"
+            >
+              {" "}
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis
+              cum quia nobis sed beatae incidunt.
+            </p>
           </div>
+        </div>
+      </div>
+
+      <div className="min-h-screen bg-[#1e3932] py-40">
+        <div className="container mx-auto flex items-center justify-center gap-24 flex-wrap">
+          {coffees.map((item) => {
+            return (
+              <div className="text-white rounded-md shadow-sm shadow-teal-600 p-6 flex flex-col items-center gap-6 transition duration-300 hover:shadow-teal-400 cursor-pointer hover:scale-95">
+                <img src={item.img} alt="" />
+                <h2 className="text-gray-300 font-medium text-2xl tracking-wide">
+                  {item.title}
+                </h2>
+                <p className="text-gray-100 font-bold text-3xl tracking-wide">
+                  {item.value} {item.price}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default App
+export default App;
---
+++ client/src/data/coffees..js
@@ -0,0 +1,26 @@
+export const coffees = [
+  {
+    img: 'https://globalassets.starbucks.com/digitalassets/products/bev/ElphabasGreenElixer.jpg?impolicy=1by1_tight_288',
+    title: 'Salted Pecan Crunch Cold Brew',
+    price: 6.99,
+    value: '$',
+  },
+  {
+    img: 'https://globalassets.starbucks.com/digitalassets/products/bev/NonDairyVanillaSweetCreamColdBrew.jpg?impolicy=1by1_tight_288',
+    title: 'Salted Pecan Crunch Cold Brew',
+    price: 7.99,
+    value: '$',
+  },
+  {
+    img: 'https://globalassets.starbucks.com/digitalassets/products/bev/SBX20210611_ColdBrew.jpg?impolicy=1by1_tight_288',
+    title: 'Salted Pecan Crunch Cold Brew',
+    price: 5.99,
+    value: '$',
+  },
+  {
+    img: 'https://globalassets.starbucks.com/digitalassets/products/bev/SaltedPecanCrunchColdFoamColdBrew.jpg?impolicy=1by1_tight_288',
+    title: 'Salted Pecan Crunch Cold Brew',
+    price: 11.99,
+    value: '$',
+  },
+];
